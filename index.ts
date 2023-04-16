905df969e2d16899957d1bd66787b4773c96f718
DanielSzczucki <117800247+DanielSzczucki@users.noreply.github.com>
2023-04-16 17:26:25 +0200

    changed variables for port

--- index.ts
+++ index.ts
@@ -3,10 +3,12 @@ dotenv.config();
 
 import express from "express";
 
+const PORT = parseInt(process.env.PORT) || 4000;
+
 const app = express();
 
 app.use(express.json());
 
-app.listen(parseInt(process.env.PORT), "0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
