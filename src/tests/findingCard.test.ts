5e89d221dfc626b22f4c5c753b5655eed60506b2
DanielSzczucki <117800247+DanielSzczucki@users.noreply.github.com>
2023-05-02 20:25:35 +0200

    added two tests

--- src/tests/findingCard.test.ts
+++ src/tests/findingCard.test.ts
@@ -1,48 +1,79 @@
-import { db } from "../utils/db";
-import { Card, cardSchema } from "../models/fischcardModel";
+import { Card } from "../models/fischcardModel";
 import { CardPayload } from "../utils/types";
-import mongoose, { ObjectId } from "mongoose";
-import fetch from "node-fetch";
+import mongoose, { Query } from "mongoose";
+import { app } from "..//../index";
+import request from "supertest";
+import { getAllCardsByQuery } from "../services/fischcard.service";
 
 const domain = process.env.CORS_DOMAIN;
-//connect to db
-beforeAll(async () => {
-  db;
+let server: any;
 
-  const firstCardData: CardPayload = {
-    _id: new mongoose.Types.ObjectId("6443eb5708dd23f4eda841c0"),
-    front: "blue fron card",
-    back: "black back card",
-    tags: ["random tag", "other random tag"],
-    author: "Testowy Author",
-    date: new Date("2023-04-27T14:44:56.012Z"),
-  };
+beforeEach((done) => {
+  server = app;
+  done();
+});
 
-  const secondCardData: CardPayload = {
-    _id: new mongoose.Types.ObjectId("6443eb5708dd23f4eda841c0"),
-    front: "orange fron card",
-    back: "yellow back card",
-    tags: ["some tag", "nice random tag"],
-    author: "Author Author",
-    date: new Date("2023-04-27T14:45:56.012Z"),
-  };
-
-  const firstCard = new Card(firstCardData);
-  const seconfCard = new Card(secondCardData);
+afterAll((done) => {
+  server.close(done);
 });
 
 describe("fishCard router", () => {
-  //   beforeEach(async () => {});
+  let firstCardMock: CardPayload;
+  let secondCardMock: CardPayload;
+  describe("GET/cards", () => {
+    beforeEach(async () => {
+      const firstCardData: CardPayload = {
+        _id: new mongoose.Types.ObjectId("6443eb5708dd23f4eda841c0"),
+        front: "blue fron card",
+        back: "black back card",
+        tags: ["random tag", "other random tag"],
+        author: "Testowy Author",
+        date: new Date("2023-04-27T14:44:56.012Z"),
+      };
 
-  it("get all card should return status code of 200", async () => {
-    //response.status
-    // expect(resultss).toBe(200);
+      const secondCardData: CardPayload = {
+        _id: new mongoose.Types.ObjectId("6443eb5708dd23f4eda841c0"),
+        front: "orange fron card",
+        back: "yellow back card",
+        tags: ["some tag", "nice random tag"],
+        author: "Author Author",
+        date: new Date("2023-04-27T14:45:56.012Z"),
+      };
+
+      firstCardMock = new Card(firstCardData);
+      secondCardMock = new Card(secondCardData);
+    });
+
+    it("get all card route, should return status code of 200", async () => {
+      const res = await request(app).get("/cards");
+
+      expect(res.status).toBe(200);
+    });
+
+    it("returns an array of flashcards in the correct order.", async () => {
+      const res = await getAllCardsByQuery({});
+
+      const firstCardDate = new Date(res[0].date);
+      const secondCardDate = new Date(res[1].date);
+
+      console.log(firstCardDate, secondCardDate);
+
+      expect(+firstCardDate).toBeGreaterThan(+secondCardDate);
+    });
+  });
+
+  it("returns the correct number of flashcards.", async () => {
+    const mockDataArr = [firstCardMock, secondCardMock];
+    const findMethodMock = jest.replaceProperty(
+      mongoose.Model,
+      "find",
+      () => mockDataArr
+    );
+
+    const cards = await Card.find({});
+    expect(cards).toHaveLength(2);
   });
 });
 
-// .
-
-// Test that the function returns an array of flashcards in the correct order.
-// Test that the function returns the correct number of flashcards.
 // Test that the function returns an array of flashcards written by the requested author in the correct order.
 // Test that the function returns the correct number of flashcards written by the requested author.
