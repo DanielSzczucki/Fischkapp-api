e76a53fe81cc69d9e6dd8a14f19eba9f3602f64f
DanielSzczucki <117800247+DanielSzczucki@users.noreply.github.com>
2023-05-05 13:51:59 +0200

    added basic test for get fischcards endpoint

--- src/tests/findingCard.test.ts
+++ src/tests/findingCard.test.ts
@@ -1,49 +1,37 @@
-import { Card } from "../models/fischcardModel";
+import { Card, cardSchema } from "../models/fischcardModel";
 import { CardPayload } from "../utils/types";
 import mongoose, { Query } from "mongoose";
 import { app } from "..//../index";
-import request from "supertest";
+import request, { Request } from "supertest";
 import { getAllCardsByQuery } from "../services/fischcard.service";
-
-const domain = process.env.CORS_DOMAIN;
-let server: any;
-
-beforeEach((done) => {
-  server = app;
-  done();
-});
-
-afterAll((done) => {
-  server.close(done);
-});
+import supertest from "supertest";
 
 describe("fishCard router", () => {
-  let firstCardMock: CardPayload;
-  let secondCardMock: CardPayload;
+  const domain = process.env.CORS_DOMAIN;
+
+  const firstCardData: CardPayload = {
+    _id: new mongoose.Types.ObjectId("6443eb5708dd23f4eda841e8"),
+    front: "silver fron card",
+    back: "black back card",
+    tags: ["random tag", "other random tag"],
+    author: "Testowy Author",
+    date: new Date("2023-04-27T14:44:56.012Z"),
+  };
+
+  const secondCardData: CardPayload = {
+    _id: new mongoose.Types.ObjectId("6443eb5708dd23f4eda841d5"),
+    author: "Other Artist",
+    back: "black back card",
+    front: "green fron card",
+    tags: ["random tag", "other random tag"],
+    date: new Date("2023-05-02T14:05:33.310Z"),
+  };
+  const firstCardMock = new Card(firstCardData);
+  const secondCardMock = new Card(secondCardData);
+
+  const initialCards = [firstCardMock, secondCardMock];
+
   describe("GET/cards", () => {
-    beforeEach(async () => {
-      const firstCardData: CardPayload = {
-        _id: new mongoose.Types.ObjectId("6443eb5708dd23f4eda841c0"),
-        front: "blue fron card",
-        back: "black back card",
-        tags: ["random tag", "other random tag"],
-        author: "Testowy Author",
-        date: new Date("2023-04-27T14:44:56.012Z"),
-      };
-
-      const secondCardData: CardPayload = {
-        _id: new mongoose.Types.ObjectId("6443eb5708dd23f4eda841c0"),
-        front: "orange fron card",
-        back: "yellow back card",
-        tags: ["some tag", "nice random tag"],
-        author: "Author Author",
-        date: new Date("2023-04-27T14:45:56.012Z"),
-      };
-
-      firstCardMock = new Card(firstCardData);
-      secondCardMock = new Card(secondCardData);
-    });
-
     it("get all card route, should return status code of 200", async () => {
       const res = await request(app).get("/cards");
 
@@ -56,24 +44,62 @@ describe("fishCard router", () => {
       const firstCardDate = new Date(res[0].date);
       const secondCardDate = new Date(res[1].date);
 
+      expect(+secondCardDate).toBeGreaterThan(+firstCardDate);
+    });
+
+    it("returns the correct number of flashcards.", async () => {
+      const response = await request(app).get("/cards");
+      console.log(response.body.cards);
+
+      expect(response.body.cards).toHaveLength(3);
+    });
+
+    it("function returns an array of flashcards written by the requested author in the correct order.", async () => {
+      const res = await request(app).get("/cards/author/artist");
+
+      const firstCardDate = new Date(res.body.cards[0].date);
+      const secondCardDate = new Date(res.body.cards[1].date);
+
       console.log(firstCardDate, secondCardDate);
 
-      expect(+firstCardDate).toBeGreaterThan(+secondCardDate);
+      expect(+secondCardDate).toBeGreaterThan(+firstCardDate);
     });
-  });
+    it(" returns the correct number of flashcards written by the requested author.", async () => {
+      const res = await request(app).get("/cards/author/artist");
 
-  it("returns the correct number of flashcards.", async () => {
-    const mockDataArr = [firstCardMock, secondCardMock];
-    const findMethodMock = jest.replaceProperty(
-      mongoose.Model,
-      "find",
-      () => mockDataArr
-    );
+      expect(res.body.cards).toHaveLength(2);
+    });
+    it("returns the correct number of flashcards with the requested tag.", async () => {
+      const res = await request(app).get("/cards/tags/other");
 
-    const cards = await Card.find({});
-    expect(cards).toHaveLength(2);
+      expect(res.body.cards).toHaveLength(2);
+    });
   });
 });
 
-// Test that the function returns an array of flashcards written by the requested author in the correct order.
-// Test that the function returns the correct number of flashcards written by the requested author.
+const rndmArray = [
+  {
+    _id: "6443eb5708dd23f4eda841c0",
+    author: "Testowy Author",
+    back: "black back card",
+    date: "2023-05-03T09:21:42.237Z",
+    front: "blue fron card",
+    tags: ["random tag", "other random tag"],
+  },
+  {
+    _id: "645118adc90ca8343942a89a",
+    author: "Other Artist",
+    back: "black back card",
+    date: "2023-05-02T14:05:33.310Z",
+    front: "green fron card",
+    tags: ["random tag", "other random tag"],
+  },
+  {
+    _id: "645118c6c90ca8343942a89e",
+    author: "Big Artist",
+    back: "black back card",
+    date: "2023-05-02T14:05:58.137Z",
+    front: "red fron card",
+    tags: ["random tag", "second tag"],
+  },
+];
