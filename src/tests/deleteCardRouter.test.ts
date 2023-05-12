b73f82ee13908189b3edf3f4745551f41794c36c
DanielSzczucki <117800247+DanielSzczucki@users.noreply.github.com>
2023-05-12 20:00:31 +0200

    added test

--- src/tests/deleteCardRouter.test.ts
+++ src/tests/deleteCardRouter.test.ts
@@ -48,8 +48,9 @@ describe("fishCard POST router", () => {
     await dropCollection();
   });
 
-  it("retrund code 204 if card deleted correctly and deletes the requested flashcard if it was created less than 5 minutes ago", async () => {
+  it("retruns code 204 if card deleted correctly and deletes the requested flashcard if it was created less than 5 minutes ago", async () => {
     let deletedCard: CardPayload;
+
     const foundCard = await Card.findById({ _id: firstCardMock._id });
 
     const currentTime = new Date();
@@ -59,32 +60,16 @@ describe("fishCard POST router", () => {
 
     if (timeDifferenceInMinutes <= 5) {
       const deletedCard = await Card.findByIdAndDelete(foundCard._id);
-      return deletedCard as CardPayload;
-    }
 
-    expect(deletedCard).toBeDefined();
-    expect(deletedCard.author).toBe(firstCardMock.author);
-    expect(deletedCard.front).toBe(firstCardMock.front);
-    expect(deletedCard.back).toBe(firstCardMock.back);
-    expect(deletedCard.tags).toStrictEqual(firstCardMock.tags);
+      expect(deletedCard).toBeDefined();
+      expect(deletedCard.author).toBe(firstCardMock.author);
+      expect(deletedCard.front).toBe(firstCardMock.front);
+      expect(deletedCard.back).toBe(firstCardMock.back);
+      expect(deletedCard.tags).toStrictEqual(firstCardMock.tags);
+    }
   });
 
   it("returns a status code of 403 if the flashcard was created more than 5 minutes ago.", async () => {
-    const deleteCardWhenTimePassed = async (
-      card: CardPayload,
-      minutes: number
-    ): Promise<CardPayload> => {
-      const currentTime = new Date();
-      const cardCreationTime = new Date(`${card.date}`);
-      const timeDifferenceInMinutes =
-        (currentTime.getTime() - cardCreationTime.getTime()) / 60000;
-
-      if (timeDifferenceInMinutes <= minutes) {
-        const deletedCard = await Card.findByIdAndDelete(card._id);
-        return deletedCard as CardPayload;
-      } else throw Error("5 miutes passed, you cant delete the post");
-    };
-
     try {
       const res = await Card.findById({ _id: secondCardMock._id });
 
@@ -99,6 +84,7 @@ describe("fishCard POST router", () => {
       const res = await Card.findByIdAndDelete({ _id: "iyfrg92hu13nvkenf1" });
     } catch (error) {
       expect(error).toBeInstanceOf(Error);
+      expect(error).toThrowError("5 miutes passed, you cant delete the post");
     }
   });
 });
