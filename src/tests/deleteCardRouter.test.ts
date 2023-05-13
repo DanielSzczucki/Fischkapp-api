04770cfe56f1b743a8cf77ef2d2a4697ce0ad88b
DanielSzczucki <117800247+DanielSzczucki@users.noreply.github.com>
2023-05-13 19:50:46 +0200

    rebuild and fix delete route tests

--- src/tests/deleteCardRouter.test.ts
+++ src/tests/deleteCardRouter.test.ts
@@ -1,20 +1,19 @@
 import { Card } from "../models/fischcardModel";
 import { CardPayload } from "../utils/types";
 import mongoose, { HydratedDocument, deleteModel } from "mongoose";
-import supertest from 'supertest';
+import supertest from "supertest";
 
 import { connectDB, dropDB, dropCollection } from "./setuptestdb";
 import { deleteCardWhenTimePassed } from "../services/fischcard.service";
+import { app } from "../../index";
 
 const supertestConfig: { [key: string]: string } = {
   Authorization: "secret",
 };
 
-import { app } from '../../index'
-
 const api = supertest(app);
 
-describe("fishCard POST router", () => {
+describe("fishCard DELETE router", () => {
   let firstCardMock: HydratedDocument<CardPayload>;
   let secondCardMock: HydratedDocument<CardPayload>;
 
@@ -57,43 +56,23 @@ describe("fishCard POST router", () => {
     await dropCollection();
   });
 
-  // it("retruns code 204 if card deleted correctly and deletes the requested flashcard if it was created less than 5 minutes ago", async () => {
-  //   let deletedCard: CardPayload;
+  it("retruns code 204 if card deleted correctly and deletes the requested flashcard if it was created less than 5 minutes ago", async () => {
+    const deletedCard = await api
+      .delete(`/cards/${firstCardMock._id}`)
+      .expect(204);
+  });
 
-  //   const foundCard = await Card.findById({ _id: firstCardMock._id });
-
-  //   const currentTime = new Date();
-  //   const cardCreationTime = new Date(`${foundCard.date}`);
-  //   const timeDifferenceInMinutes =
-  //     (currentTime.getTime() - cardCreationTime.getTime()) / 60000;
-
-  //   if (timeDifferenceInMinutes <= 5) {
-  //     const deletedCard = await Card.findByIdAndDelete(foundCard._id);
-
-  //     expect(deletedCard).toBeDefined();
-  //     expect(deletedCard.author).toBe(firstCardMock.author);
-  //     expect(deletedCard.front).toBe(firstCardMock.front);
-  //     expect(deletedCard.back).toBe(firstCardMock.back);
-  //     expect(deletedCard.tags).toStrictEqual(firstCardMock.tags);
-  //   }
-  // });
-
-  // it("returns a status code of 403 if the flashcard was created more than 5 minutes ago.", async () => {
-  //   try {
-  //     const res = await Card.findById({ _id: secondCardMock._id });
-
-  //     const deletedCard = await deleteCardWhenTimePassed(res, 5);
-  //   } catch (error) {
-  //     expect(error).toBeInstanceOf(Error);
-  //   }
-  // });
+  it("returns a status code of 403 if the flashcard was created more than 5 minutes ago.", async () => {
+    const deletedCard = await api
+      .delete(`/cards/${secondCardMock._id}`)
+      .expect(403);
+  });
 
   it("returns a status code of 404 if the requested flashcard does not exist.", async () => {
-        const card = await Card.findById({ _id: "6445eb5708dd23f4eda842e9" });
+    const card = await Card.findById("6413eb5708dd23f4eda841e8");
 
-        expect(card).not.toBeTruthy()
+    expect(card).not.toBeTruthy();
 
-        // You need to debug why your app returns 500 here, it should be 404
-        await api.delete(`/cards/${'123'}`).expect(404);
+    await api.delete(`/cards/${"6447eb5708dd23f4eda841e8"}`).expect(404);
   });
 });
