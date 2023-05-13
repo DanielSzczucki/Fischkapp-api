2663debc83944b7035a1488c4187591960039754
DanielSzczucki <117800247+DanielSzczucki@users.noreply.github.com>
2023-05-13 19:46:01 +0200

    added test environement to connect with mock db

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -4,12 +4,11 @@ const DB_NAME = process.env.DB_NAME || "localdb";
 
 export let db: Connection;
 
-if (process.env.NODE_ENV !== 'test') {
-    mongoose.connect(`${DB_NAME}`);
-    db = mongoose.connection;
-    db.on("error", console.error.bind(console, "connection error"));
-    db.once("open", () => {
-      console.log("Connected to db");
-    });
+if (process.env.NODE_ENV !== "test") {
+  mongoose.connect(`${DB_NAME}`);
+  db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error"));
+  db.once("open", () => {
+    console.log("Connected to db");
+  });
 }
-
