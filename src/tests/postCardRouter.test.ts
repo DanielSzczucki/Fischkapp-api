f242abcfd5f98219ff48953ef3b7a28660bb1fd2
DanielSzczucki <117800247+DanielSzczucki@users.noreply.github.com>
2023-05-13 20:53:12 +0200

    rebuild post router tests

--- src/tests/postCardRouter.test.ts
+++ src/tests/postCardRouter.test.ts
@@ -1,13 +1,37 @@
-import { Card, cardSchema } from "../models/fischcardModel";
-import { CardPayload, CreateCardPayload } from "../utils/types";
+import { Card } from "../models/fischcardModel";
+import { CardPayload } from "../utils/types";
 import mongoose, { HydratedDocument } from "mongoose";
-
 import { connectDB, dropDB, dropCollection } from "./setuptestdb";
-import { Request, Response } from "express";
+
+import supertest from "supertest";
+import { app } from "../../index";
+
+const supertestConfig: { [key: string]: string } = {
+  Authorization: "secret",
+};
+
+const api = supertest(app);
 
 describe("fishCard POST router", () => {
   let firstCardMock: HydratedDocument<CardPayload>;
   let secondCardMock: HydratedDocument<CardPayload>;
+
+  const firstCardData: object = {
+    _id: "6443eb5708dd23f4eda841e8",
+    author: "Author test",
+    front: "silver fron card",
+    back: "black back card",
+    tags: ["random tag", "other random tag"],
+    date: "2023-04-27T14:44:56.012Z",
+    __v: 0,
+  };
+
+  const secondCardData = {
+    author: "Author Other ",
+    back: "black back card",
+    front: "green fron card",
+    tags: ["random tag", "other random tag"],
+  };
 
   beforeAll(async () => {
     await connectDB();
@@ -18,24 +42,12 @@ describe("fishCard POST router", () => {
   });
 
   beforeEach(async () => {
-    const firstCardData = {
-      _id: new mongoose.Types.ObjectId("6443eb5708dd23f4eda841e8"),
-      author: "Author test",
-      front: "silver fron card",
-      back: "black back card",
-      tags: ["random tag", "other random tag"],
-      date: new Date("2023-04-27T14:44:56.012Z"),
-      __v: 0,
-    };
-
-    const secondCardData = {
-      author: "Author Other ",
-      back: "black back card",
-      front: "silver fron card",
-      tags: ["random tag", "other random tag"],
-    };
+    //create cards
     firstCardMock = new Card(firstCardData);
     secondCardMock = new Card(secondCardData);
+
+    //added one to db
+    firstCardMock.save();
   });
 
   afterEach(async () => {
@@ -43,23 +55,18 @@ describe("fishCard POST router", () => {
   });
 
   it("returns a status code of 201", async () => {
-    const res = await firstCardMock.save();
-
-    expect(res).toBeDefined();
+    await api.post(`/cards`).send(secondCardData).expect(201);
   });
 
   it("create a new flashcard with the correct fields", async () => {
-    const res = await firstCardMock.save();
+    const res = await api.post(`/cards`).send(firstCardData);
 
-    expect(res).toBeInstanceOf(Card);
-    expect(res).toMatchObject(firstCardMock);
+    const card = res.body.card;
+
+    expect(card).toMatchObject(firstCardData);
   });
 
   it(" returns a status code of 400 when card with specific front value already exists", async () => {
-    const res = await firstCardMock.save();
-    const res2 = await secondCardMock.save();
-
-    //returned existing card
-    expect(res2.front).toBe(res.front);
+    await api.post(`/cards`).send(firstCardData).expect(400);
   });
 });
